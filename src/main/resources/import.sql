1c13c3f983f5eb1e9be3e3e9366e7ac1809de1eb
Pablo Jonhatan Solis Mendiola <pablosolismendiola@gmail.com>
2023-11-27 11:14:00 -0600

    Agrega 'import.sql' para generar datos de prueba

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+
+/* Inserta en la tabla clientes */
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Pablo','Solis','pablo.solis@example.com','2023-06-11');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Victor','Solis','victor.solis@example.com','2023-06-11');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Carlos','Rodriguez','carlos.rodriguez@example.com','2023-06-11');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Brandon','Rodriguez','brandon.rodriguez@example.com','2023-06-11');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Benjamin','Aguilera','benjamin.aguilera@example.com','2023-06-11');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Yolanda','Sierra','yolanda.sierra@example.com','2023-06-11');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Leslie','Issa','leslie.issa@example.com','2023-06-11');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Angela','Alvarez','angela.alvarez@example.com','2023-06-11');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Diego','Trujillo','diego.trujillo@example.com','2023-06-11');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Oliver','Garza','oliver.garza@example.com','2023-06-11');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Jennifer','Flores','jennifer.flores@example.com','2023-06-11');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Dulce','Torres','dulce.torres@example.com','2023-06-11');
