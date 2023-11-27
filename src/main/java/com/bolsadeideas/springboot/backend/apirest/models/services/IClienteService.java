44d7ac13208315a49169bf0ba369083f3fd14292
Pablo Jonhatan Solis Mendiola <pablosolismendiola@gmail.com>
2023-11-27 11:19:40 -0600

    Add 'findById' method to the contract

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
@@ -1,10 +1,13 @@
 package com.bolsadeideas.springboot.backend.apirest.models.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 
 public interface IClienteService
 {
     public List<Cliente> findAll();
+
+    public Optional<Cliente> findById(Long id);
 }
