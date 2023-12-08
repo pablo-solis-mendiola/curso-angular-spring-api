1a80af4d76b8efaace6100bbc9a13eba0c6ee43b
Pablo Jonhatan Solis Mendiola <pablosolismendiola@gmail.com>
2023-12-08 05:35:34 -0600

    Agrega logica en caso de que el cliente no sea encontrado

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,18 +1,27 @@
 package com.bolsadeideas.springboot.backend.apirest.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 import com.bolsadeideas.springboot.backend.apirest.models.services.IClienteService;
+
+import exceptions.ClienteNotFoundException;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api")
@@ -28,7 +37,46 @@ public class ClienteRestController {
     }
 
     @GetMapping("/clientes/{id}")
-    public Optional<Cliente> show(@PathVariable Long id) {
-        return clienteService.findById(id);
+    public Cliente show(@PathVariable Long id) {
+        Cliente cliente = clienteService.findById(id);
+
+        if(cliente == null) {
+            throw new ClienteNotFoundException();
+        }
+
+        return cliente;
+    }
+
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente store(@RequestBody Cliente cliente) {
+        return clienteService.save(cliente);
+    }
+
+    @PutMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente update(@PathVariable Long id, @RequestBody Cliente cliente) {
+        Cliente clienteExistente = clienteService.findById(id);
+
+        if(clienteExistente == null) {
+            throw new ClienteNotFoundException();
+        }
+
+        String nombre = (cliente.getNombre() == null)? clienteExistente.getNombre() : cliente.getNombre();
+        String apellido = (cliente.getApellido() == null)? clienteExistente.getApellido() : cliente.getApellido();
+        String email = (cliente.getEmail() == null)? clienteExistente.getEmail() : cliente.getEmail(); 
+
+        clienteExistente.setNombre(nombre);
+        clienteExistente.setApellido(apellido);
+        clienteExistente.setEmail(email);
+        clienteService.save(clienteExistente);
+
+        return clienteExistente;
+    }
+
+    @DeleteMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void destroy(@PathVariable Long id) {
+        clienteService.deleteById(id);
     }
 }
